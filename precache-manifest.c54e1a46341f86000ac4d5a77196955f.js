68c90f990c34742616d11eb4011017312627cf91
diferpaho <diferpaho@gmail.com>
2020-05-25 15:23:36 -0500

    Updates

---
+++ precache-manifest.c54e1a46341f86000ac4d5a77196955f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf30c9c42c10747205c1482eb555a9e8",
+    "url": "/front/index.html"
+  },
+  {
+    "revision": "dbb8653d41ee67ab3773",
+    "url": "/front/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f5ad0cb23004772cb0c9",
+    "url": "/front/static/css/main.40d7170d.chunk.css"
+  },
+  {
+    "revision": "dbb8653d41ee67ab3773",
+    "url": "/front/static/js/2.f34e7d1a.chunk.js"
+  },
+  {
+    "revision": "6d98c309ab0e4d5aa5d30f35f2390953",
+    "url": "/front/static/js/2.f34e7d1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5ad0cb23004772cb0c9",
+    "url": "/front/static/js/main.c5a154e5.chunk.js"
+  },
+  {
+    "revision": "59314b4e98484a631624",
+    "url": "/front/static/js/runtime-main.50c1285a.js"
+  }
+]);
